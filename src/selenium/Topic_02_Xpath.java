eba48d8e7b9c7619f9ac1288e6df5f99ea49393a
NguyenThiThuy <nguyenthithuy8497@gmail.com>
2018-11-02 11:13:30 +0700

    Finish Topic 02

---
+++ src/selenium/Topic_02_Xpath.java
@@ -0,0 +1,19 @@
+package selenium;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Topic_02_Xpath {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
